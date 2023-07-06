8f311b441d35ba806860773f2446cd921fd348ae
Ggaggamel <r.frankenbytes@gmaile.com>
2023-07-06 18:32:45 +0800

    updating App.js for new implemented components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Fragment } from 'react';
+import React from 'react';
 import './App.css';
+import Header from "./components/Header.jsx"
+import Navbar from './components/Navbar.jsx';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <Fragment>
+    <Header></Header>
+    <Navbar></Navbar>
+    <Main></Main>
+    <Footer></Footer>
     <main className="App">
       <h1>LilLemon App under construction</h1>
     </main>
+    </Fragment>
   );
 }
 
