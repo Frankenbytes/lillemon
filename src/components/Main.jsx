b90fc1736212448bba1bc29df443fd9e34993b4b
Ggaggamel <r.frankenbytes@gmaile.com>
2023-07-06 18:30:43 +0800

    implementing Main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    state = {  } 
+    render() { 
+        return <main>This is the main area</main>;
+    }
+}
+ 
+export default Main;
