1e8dbc14e6b39b22ed6b96f183805b949d9b509d
Ggaggamel <r.frankenbytes@gmaile.com>
2023-07-06 19:50:56 +0800

    Add Logo and Nav Menu Ul

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     state = {  } 
     render() { 
-        return <nav>This is the Navbar</nav>;
+        return <nav>
+                    <img src="./Logo.svg" className="App-logo" alt="logo" />
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Menu</li>
+                        <li>Reservation</li>
+                        <li>Order online</li>
+                        <li>Login</li>
+                    </ul>
+        </nav>;
     }
 }
  
