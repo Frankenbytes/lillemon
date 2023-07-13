fb3609e31a4d4b49b6d4ae2ef245886031b03790
Ggaggamel <r.frankenbytes@gmaile.com>
2023-07-13 17:12:58 +0800

    Highlights component including full style

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Bruschetta from './bruchetta.png';
+import GreekSalad from './greek salad.jpg';
+import LemonDessert from './lemon dessert.jpg';
 
 
 class Highlights extends Component {
@@ -15,10 +18,52 @@ class Highlights extends Component {
                     </section>
                     <section className='highlightsBottom'>
                         <article className='highlightCart'>
+                            <img src={GreekSalad} className='cartPic'></img>
+                            <section className='cartBody'>
+                            <section className='cartHead'>
+                                <h3 className='dishName'>Greek Salad</h3>
+                                <h3 className='cartPrice'>$12.99</h3>
+                            </section>
+                            <p className='cartDescription'>The famous greek salad of<br></br>
+                            crispy lettuce, peppers, olives <br></br>
+                            and our Chicago style feta<br></br>
+                            cheese, garnished with <br></br>
+                            crunchy garlic and rosemary<br></br>
+                            croutons.</p>
+                            <a className='orderLink'><p>Order a delivery</p></a>
+                            </section>
                         </article>
                         <article className='highlightCart'>
+                            <img src={Bruschetta} className='cartPic'></img>
+                            <section className='cartBody'>
+                            <section className='cartHead'>
+                                <h3 className='dishName'>Bruschetta</h3>
+                                <h3 className='cartPrice'>$5.99</h3>
+                            </section>
+                            <p className='cartDescription'>The famous greek salad of<br></br>
+                            crispy lettuce, peppers, olives <br></br>
+                            and our Chicago style feta<br></br>
+                            cheese, garnished with <br></br>
+                            crunchy garlic and rosemary<br></br>
+                            croutons.</p>
+                            <a className='orderLink'><p>Order a delivery</p></a>
+                            </section>
                         </article>
                         <article className='highlightCart'>
+                            <img src={LemonDessert} className='cartPic'></img>
+                            <section className='cartBody'>
+                            <section className='cartHead'>
+                                <h3 className='dishName'>Lemon Dessert</h3>
+                                <h3 className='cartPrice'>$5.00</h3>
+                            </section>
+                            <p className='cartDescription'>The famous greek salad of<br></br>
+                            crispy lettuce, peppers, olives <br></br>
+                            and our Chicago style feta<br></br>
+                            cheese, garnished with <br></br>
+                            crunchy garlic and rosemary<br></br>
+                            croutons.</p>
+                            <a className='orderLink'><p>Order a delivery</p></a>
+                            </section>
                         </article>
                     </section>
         </section>
