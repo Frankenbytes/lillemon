5afa17b52882133c9987111511142d30eeb69ac5
ralVovax <141141823+ralVovax@users.noreply.github.com>
2023-08-16 18:48:59 +0800

    final commit after offline and laptop change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import { Fragment } from 'react';
-import React from 'react';
-import './App.css';
-import Header from "./components/Header.jsx"
-import Navbar from './components/Navbar.jsx';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import React from "react";
+import Home from "./routes/Home";
+import Reservations from "./routes/Reservations";
+
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <Fragment>
-    <Header></Header>
-    <Navbar></Navbar>
-    <Main></Main>
-    <Footer></Footer>
-    <main className="App">
-      <h1>LilLemon App under construction</h1>
-    </main>
-    </Fragment>
+    <>
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/reservations" element = {<Reservations />}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import "../components/styles/Footer.css";
+import Logo from "../components/assets/logo.png";
+
+const Footer = () => {
+  return (
+    <div>
+        <div className="footer-background">
+            <div className="footer-container">
+                <img src={ Logo } alt="Little Lemon logo" />
+                <p>Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. The restaurant features a locally sourced menu with daily specials.</p>
+                <div className="footer-content">
+                    <div className="locations">
+                        <h5>LOCATIONS</h5>
+                        <ul>
+                            <li>North Town</li>
+                            <li>East Cape</li>
+                            <li>south Coast</li>
+                            <li>West Cape</li>
+                        </ul>
+                    </div>
+                    <div className="opening-times">
+                        <h5>OPENING TIMES</h5>
+                        <ul>
+                            <li>Mon - Wed: 10:00AM - 11:00AM</li>
+                            <li>Fri: 10:00PM - 01:00AM</li>
+                            <li>Sat - Sun: 09:30AM - 02:00AM</li>
+                        </ul>
+                    </div>
+                    <div className="contact">
+                        <h5>CONTACT US</h5>
+                        <ul>
+                            <li>96 Normal Road, West Cape - Albuquerque</li>
+                            <li>Tel: 0212 786 52 72</li>
+                            <li>Email: info@littlelemon.com</li>
+                        </ul>
+                    </div>
+            </div>
+            <div className="copyright">
+                <p>© 2023 Little Lemon Ltd. All rights reserved.</p>
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
